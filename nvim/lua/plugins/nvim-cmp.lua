00c1f52d89041d082181e1e42977f475cef63c05
Satyam-2004 <skpsharma2004@gmail.com>
2026-01-20 08:54:17 +0530

    changed hotkeys in nvim-cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,66 +1,109 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-			dependencies = {},
-		},
-		"saadparwaiz1/cmp_luasnip",
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    {
+      "L3MON4D3/LuaSnip",
+      build = (function()
+        if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+          return
+        end
+        return "make install_jsregexp"
+      end)(),
+      dependencies = { "saadparwaiz1/cmp_luasnip" },
+    },
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+  },
 
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-nvim-lsp-signature-help",
-	},
-	config = function()
-		if not vim.g.vscode then
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			luasnip.config.setup({})
+  config = function()
+    if vim.g.vscode then return end   -- skip in vscode-neovim
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				completion = { completeopt = "menu,menuone,noinsert" },
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
-				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = cmp.mapping.select_next_item(),
-					["<S-Tab>"] = cmp.mapping.select_prev_item(),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete({}),
-					["<C-l>"] = cmp.mapping(function()
-						if luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						end
-					end, { "i", "s" }),
-					["<C-h>"] = cmp.mapping(function()
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						end
-					end, { "i", "s" }),
-				}),
-				sources = {
-					{
-						name = "lazydev",
-						group_index = 0,
-					},
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "path" },
-					{ name = "nvim_lsp_signature_help" },
-				},
-			})
-		end
-	end,
+    luasnip.config.setup({})
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+
+      completion = {
+        completeopt = "menu,menuone,noinsert",   -- most people prefer noinsert
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        -- Super-Tab style: navigate + smart confirm
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You can swap the next two lines depending on preference:
+            -- Option A: Always confirm on Tab (very VSCode-like)
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+            
+            -- Option B: Select next → only confirm when you want (more conservative)
+            -- cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()   -- normal indent / tab when no completion
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        -- Classic scroll documentation
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+        -- Manual trigger completion (optional but useful)
+        ["<C-Space>"] = cmp.mapping.complete(),
+
+        -- Enter = NEW LINE (very important for most coders!)
+        ["<CR>"] = cmp.mapping({
+          i = function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            else
+              fallback()  -- normal <CR>
+            end
+          end,
+          s = cmp.mapping.confirm({ select = true }),   -- snippet mode
+          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
+
+        -- Keep your luasnip navigation (can remove if you prefer Tab only)
+        -- ["<C-l>"] = cmp.mapping(function()
+        --   if luasnip.expand_or_locally_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   end
+        -- end, { "i", "s" }),
+        --
+        -- ["<C-h>"] = cmp.mapping(function()
+        --   if luasnip.locally_jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   end
+        -- end, { "i", "s" }),
+      }),
+
+      sources = {
+        { name = "lazydev", group_index = 0 },   -- optional
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "nvim_lsp_signature_help" },
+      },
+    })
+  end,
 }
