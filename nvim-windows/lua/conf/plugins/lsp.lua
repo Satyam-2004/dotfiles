95bbad166902afafd83809f5fc0427e75f61a5e9
Satyam Pravinkumar Sharma <153253105+Satyam-2004@users.noreply.github.com>
2026-06-26 17:13:48 +0530

    Working nvim-windows

--- nvim-windows/lua/conf/plugins/lsp.lua
+++ nvim-windows/lua/conf/plugins/lsp.lua
@@ -1,149 +1,53 @@
-print("LSP FILE LOADED")
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/nvim-cmp",
-  },
+    {
+        "neovim/nvim-lspconfig",
+        version = "v2.10.0",
 
-  config = function()
-    vim.api.nvim_create_autocmd('LspAttach', {
-      desc = 'LSP actions',
-      callback = function(event)
-        local opts = {buffer = event.buf}
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+        },
 
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', '<leader>k', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-      end,
-    })
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+            vim.lsp.config("clangd", {
+                cmd = {
+                    "C:/msys64/ucrt64/bin/clangd.exe",
+                    "--background-index",
+                    "--clang-tidy",
+                },
 
-    -- Mason setup
-    require("mason").setup()
+                capabilities = capabilities,
 
-    -- Mason LSP Config
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "pyright",
-        "clangd",
-        "gopls",
-      },
-    })
+                root_markers = {
+                    ".git",
+                    ".clangd",
+                    "compile_commands.json",
+                    "compile_flags.txt",
+                },
+            })
 
-    -- Mason-lspconfig setup
-    require("mason-lspconfig").setup({
-        -- Default handler for automatically configuring installed servers
-      function(server_name)
-        require("lspconfig")[server_name].setup({
-          capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        }) end,
-        -- You can add custom handlers for specific servers here
-        -- For example:
-        -- tsserver = function(_, opts)
-        --   require("lspconfig").tsserver.setup(opts)
-        -- end,
-    })
+            vim.lsp.enable("clangd")
 
-    local cmp = require("cmp")
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(event)
+                    local opts = { buffer = event.buf, silent = true }
 
-    --   פּ ﯟ   some other good icons
-    local kind_icons = {
-      Text = "",
-      Method = "m",
-      Function = "",
-      Constructor = "",
-      Field = "",
-      Variable = "",
-      Class = "",
-      Interface = "",
-      Module = "",
-      Property = "",
-      Unit = "",
-      Value = "",
-      Enum = "",
-      Keyword = "",
-      Snippet = "",
-      Color = "",
-      File = "",
-      Reference = "",
-      Folder = "",
-      EnumMember = "",
-      Constant = "",
-      Struct = "",
-      Event = "",
-      Operator = "",
-      TypeParameter = "",
-    }
-    -- find more here: https://www.nerdfonts.com/cheat-sheet
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
 
-    cmp.setup({
-      sources = {
-        {name = "nvim_lsp"},
-      },
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 
-     mapping = {
-       ["<C-k>"] = cmp.mapping.select_prev_item(),
-       ["<C-j>"] = cmp.mapping.select_next_item(),
-       ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-       ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-       ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-       ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-       ["<C-e>"] = cmp.mapping {
-         i = cmp.mapping.abort(),
-         c = cmp.mapping.close(),
-       },
-       -- Accept currently selected item. If none selected, `select` first item.
-       -- Set `select` to `false` to only confirm explicitly selected items.
-       ["<CR>"] = cmp.mapping.confirm { select = false },
-       ["<Tab>"] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-           cmp.select_next_item()
-         else
-           fallback()
-         end
-       end, {
-         "i",
-         "s",
-       }),
-       ["<S-Tab>"] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-           cmp.select_prev_item()
-         else
-           fallback()
-         end
-       end, {
-         "i",
-         "s",
-       }),
-     },
-
-    formatting = {
-      fields = { "kind", "abbr", "menu" },
-      format = function(entry, vim_item)
-        -- Kind icons
-        vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-        -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-        vim_item.menu = ({
-          nvim_lsp = "[LSP]",
-          --nvim_lsp_signature_help = "[LSP-Signature]",
-          buffer = "[Buffer]",
-          path = "[Path]",
-        })[entry.source.name]
-        return vim_item
-      end,
-    }
-  })
-  end
+                    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+                end,
+            })
+        end,
+    },
 }
-
